388c550de91c47d350d88c22dcb1cd092d18d8d8
BowersIndustry <Bowersindustry.bwrs@gmail.com>
2023-02-08 11:00:23 -0600

    Update to add message on member join

--- src/events/client/guildMemberAdd.js
+++ src/events/client/guildMemberAdd.js
@@ -1,12 +1,22 @@
+const { EmbedBuilder } = require("discord.js");
 const env = require("../../../env.json");
 
 module.exports = {
   name: "guildMemberAdd",
   async execute(member, client) {
-    let guild = client.guilds.cache.get(env.Guild_ID);
-    let memRole = guild.roles.cache.get(env.Member_Role_ID);
-    let annoRole = guild.roles.cache.get(env.Announcement_Role_ID);
-    member.roles.add(memRole);
-    member.roles.add(annoRole);
+    let guild = await client.guilds.cache.get(env.Guild_ID);
+    let memRole = await guild.roles.cache.get(env.Member_Role_ID);
+    let annoRole = await guild.roles.cache.get(env.Announcement_Role_ID);
+    await member.roles.add(memRole);
+    await member.roles.add(annoRole);
+    const embed = new EmbedBuilder()
+      .setTitle("Welcome!")
+      .setDescription(`Welcome to **Cross Crusaders**!\nWe are so glad you are here! Check out and select your roles from <#${env.Roles_Channel_ID}>. Our <@${env.Staff_Role_ID}> will be here soon to welcome you. Have fun in Cross Crusaders!`)
+      .setColor("#c3e1e1")
+    let genChnl = await client.channels.cache.get(env.General_Channel_ID);
+    genChnl.send({
+      content: `<@${member.id}>`,
+      embeds: [embed]
+    });
   }
 }
--- src/events/client/messageCreate.js
+++ src/events/client/messageCreate.js
@@ -1,4 +1,3 @@
-const { ActivityType } = require("discord.js")
 const env = require("../../../env.json");
 
 var admin = require("firebase-admin");
@@ -13,30 +12,34 @@ const topicName = 'industry-tech';
 module.exports = {
     name: "messageCreate",
     async execute(message, client) {
-        if (message.channel.id == env.SA_Updates_Channel_ID && !message.author.bot) {
-            var msgBody = message.content.split("-Body-")[1].trim();
-            var msgTitle = message.content.split("-Body-")[0].split("-Title-")[1].trim();
+        try {
+            if (message.channel.id == env.SA_Updates_Channel_ID && !message.author.bot) {
+                var msgBody = message.content.split("-Body-")[1].trim();
+                var msgTitle = message.content.split("-Body-")[0].split("-Title-")[1].trim();
 
-            const firebaseMessage = {
-                notification: {
-                    title: msgTitle,
-                    body: msgBody
-                },
-                android: {
+                const firebaseMessage = {
                     notification: {
-                        imageUrl: 'https://foo.bar.pizza-monster.png'
-                    }
-                },
-                topic: "updates"
-            };
-            await admin.messaging().send(firebaseMessage)
-                .then((response) => {
-                    message.reply("Notification sent!");
-                })
-                .catch((error) => {
-                    message.reply("There was an error sending the notification.");
-                    console.log('Error sending message:', error);
-                });
+                        title: msgTitle,
+                        body: msgBody
+                    },
+                    android: {
+                        notification: {
+                            imageUrl: 'https://foo.bar.pizza-monster.png'
+                        }
+                    },
+                    topic: "updates"
+                };
+                await admin.messaging().send(firebaseMessage)
+                    .then((response) => {
+                        message.reply("Notification sent!");
+                    })
+                    .catch((error) => {
+                        message.reply("There was an error sending the notification.");
+                        console.log('Error sending message:', error);
+                    });
+            }
+        } catch (err) {
+
         }
     }
 }
